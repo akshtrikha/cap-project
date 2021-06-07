ec253238efe64a145f3beeb0af1f5caf8fa59eb4
Akshit Trikha <akshittrikha01@gmail.com>
2021-06-07 10:37:10 +0000

    upd: Add app.py and Dockerfile

---
+++ app.py
@@ -0,0 +1,10 @@
+# Simple Hello World flask app
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+if __name__ == "__main__":
+    app.run()
